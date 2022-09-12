d13079beb8968bb6c1c694b6201444e994158f97
rdwnaden <ridwanaden1@gmail.com>
2022-09-12 22:30:59 +0700

    simple clock

---
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textClock, textDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textClock = findViewById(R.id.clock);
+        textDate = findViewById(R.id.date);
+
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                @SuppressLint("SimpleDateFormat")
+                DateFormat clockFormat = new SimpleDateFormat("HH:mm:ss");
+
+                @SuppressLint("SimpleDateFormat")
+                DateFormat dateFormat = new SimpleDateFormat("EEEE, dd MMMM yyyy");
+
+                textClock.setText(clockFormat.format(new Date()));
+                textDate.setText(dateFormat.format(new Date()));
+
+
+                //interval
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
